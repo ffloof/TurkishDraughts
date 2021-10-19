483db812d400cb555ada120de3e372bb6c67da44
Afel <prteza@gmail.com>
2021-10-19 14:14:10 -0700

    Fixed pawn promotion bug

--- Board/moves.go
+++ Board/moves.go
@@ -1,42 +1,50 @@
 package board
 
-func (bs *BoardState) AllMovesTile(x int, y int) []BoardState {
+func (bs *BoardState) AllMovesKing(x int, y int) []BoardState {
 	boards := []BoardState{}
 	checkingTile, _ := bs.GetBoardTile(x,y)
-	if checkingTile.King {
-		for _, direction := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} { //Right
-			for i:=1;i<8;i++ {
-				move := [2]int{direction[0]*i, direction[1]*i}
-				moveTile, onBoard := bs.GetBoardTile(x + move[0],y + move[1])
-				if moveTile.Team == Empty && onBoard {
-					newBS := *bs
-					newBS.SetBoardTile(x + move[0], y + move[1], checkingTile)
-					newBS.SetBoardTile(x, y, Tile{})
-					boards = append(boards, newBS)
-				} else {
-					break //Stops going in this direction after it hits wall or piece
-				}
-			}
-		}
-	} else {
-		for _, move := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} {
-			if checkingTile.Team == White && move[0] == 0 && move[1] == 1 { continue }
-			if checkingTile.Team == Black && move[0] == 0 && move[1] == -1 { continue }
-			
+
+	for _, direction := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} { //Right
+		for i:=1;i<8;i++ {
+			move := [2]int{direction[0]*i, direction[1]*i}
 			moveTile, onBoard := bs.GetBoardTile(x + move[0],y + move[1])
 			if moveTile.Team == Empty && onBoard {
 				newBS := *bs
-				if moveTile.Team == Black && y + move[1] == 0 { //Promote to king condition
-					checkingTile.King = true
-				} else if moveTile.Team == White && y + move[1] == 7 {
-					checkingTile.King = true
-				}
 				newBS.SetBoardTile(x + move[0], y + move[1], checkingTile)
 				newBS.SetBoardTile(x, y, Tile{})
 				boards = append(boards, newBS)
+			} else {
+				break //Stops going in this direction after it hits wall or piece
 			}
 		}
 	}
+
+	return boards
+}
+
+func (bs *BoardState) AllMovesPawn(x int, y int) []BoardState {
+	boards := []BoardState{}
+	checkingTile, _ := bs.GetBoardTile(x,y)
+
+	for _, move := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} {
+		if checkingTile.Team == White && move[0] == 0 && move[1] == 1 { continue }
+		if checkingTile.Team == Black && move[0] == 0 && move[1] == -1 { continue }
+		
+		moveTile, onBoard := bs.GetBoardTile(x + move[0],y + move[1])
+		if moveTile.Team == Empty && onBoard {
+			newBS := *bs
+			king := checkingTile.King
+			if checkingTile.Team == White && y + move[1] == 0 { //Promote to king condition
+				king = true
+			} else if checkingTile.Team == Black && y + move[1] == 7 {
+				king = true
+			}
+			newBS.SetBoardTile(x + move[0], y + move[1], Tile{checkingTile.Team, king})
+			newBS.SetBoardTile(x, y, Tile{})
+			boards = append(boards, newBS)
+		}
+	}
+
 	return boards
 }
 
@@ -45,8 +53,11 @@ func (bs *BoardState) AllMoveBoards(turnTeam Team) []BoardState {
 	for y := 0; y<8; y++ {
 		for x := 0; x<8; x++ {
 			if turnTeam != bs[(y*8)+x].Team { continue }
-			//TODO: if == bs[(y*8)+x].King {} else {}
-			possibleMoveBoards = append(possibleMoveBoards, bs.AllMovesTile(x, y)...)
+			if bs[(y*8)+x].King {
+				possibleMoveBoards = append(possibleMoveBoards, bs.AllMovesKing(x, y)...)
+			} else {
+				possibleMoveBoards = append(possibleMoveBoards, bs.AllMovesPawn(x, y)...)
+			}
 		}
 	}
 	return possibleMoveBoards
--- main.go
+++ main.go
@@ -7,36 +7,15 @@ import (
 
 func main() {
 	fmt.Println("Started")
-	b := board.BoardFromStr("-------- bbbbbbbb bbbbbbbb -------- -------- wwwwwwww wwwwwwww --------")
-	value := b.BoardValue(12, -board.AlphaBetaMax, board.AlphaBetaMax, board.White)
-
-	/*
-
-	options := b.MaxTakeBoards(turnTeam)
-	if len(options) == 0 {
-		options = b.AllMoveBoards(turnTeam)
-	}
-
-	var bestValue float64
-	var bestBranch BoardState
+	b := board.BoardFromStr("-------- --W---w- -------- -------- -------- -------- -------- --------")
+	
+	options := b.AllMoveBoards(board.White)
 
 	for i, branch := range options{
-		if turnTeam == White {
-			value, _ := branch.BoardValue(depth-1, Black)
-			if i==0 || value >= bestValue {
-				bestValue = value //White tries to maximize value
-				bestBranch = branch
-			}
-		} else if turnTeam == Black {
-			value, _ := branch.BoardValue(depth-1, White)
-			if i==0 || value <= bestValue {
-				bestValue = value //Black tries to minimize value
-				bestBranch = branch
-			}
-		}
-	}*/
-
-	fmt.Println(value)
+		fmt.Println()
+		fmt.Println(i)
+		branch.Print()
+	}
 }
 
 //TODO: add unit tests
