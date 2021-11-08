ded7f4c378403e1d833d7244e7ad6a61075f8b2e
Afel <prteza@gmail.com>
2021-11-07 17:56:51 -0800

    Time to write a ui

---
+++ UI/ui.go
@@ -0,0 +1,126 @@
+package ui
+
+import (
+	"TurkishDraughts/Board"
+
+	"image/color"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+const (
+	Width = 1600
+	Height = 900
+)
+
+var (
+	Border = 20.0
+	Gaps = 6.0
+	BoardBg = color.RGBA{0xED, 0xEB, 0xE9, 0xFF}
+	TileA = color.RGBA{0xB5, 0x88, 0x63, 0xFF}
+	TileB = color.RGBA{0xF0, 0xD9, 0xB5, 0xFF}
+
+	InternalGap = 10.0
+)
+
+
+func Init() {
+	b := board.CreateStartingBoard()
+
+	cfg := pixelgl.WindowConfig{
+		Title:  "Turkish Draughts Engine",
+		Bounds: pixel.R(0, 0, Width, Height),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+	imd.Color = colornames.Blueviolet
+	imd.EndShape = imdraw.RoundEndShape
+	imd.Push(pixel.V(100, 100), pixel.V(700, 100))
+	imd.EndShape = imdraw.SharpEndShape
+	imd.Push(pixel.V(100, 500), pixel.V(700, 500))
+	imd.Line(30)
+
+	imd.Color = colornames.Limegreen
+	imd.Push(pixel.V(500, 500))
+	imd.Circle(300, 50)
+
+	imd.Color = colornames.Navy
+	imd.Push(pixel.V(200, 500), pixel.V(800, 500))
+	imd.Ellipse(pixel.V(120, 80), 0)
+
+	imd.Color = colornames.Red
+	imd.EndShape = imdraw.RoundEndShape
+	imd.Push(pixel.V(500, 350))
+	imd.CircleArc(150, -math.Pi, 0, 30)
+	*/
+
+	for !win.Closed() {
+		imd := imdraw.New(nil)
+		win.Clear(colornames.Aliceblue)
+		
+		drawBoard(imd)
+		drawPieces(&b, imd)
+
+		imd.Draw(win)
+		win.Update()
+	}
+}
+
+func drawBoard(imd *imdraw.IMDraw){
+	imd.Color = BoardBg
+	imd.Push(pixel.V(0.0,0.0), pixel.V(float64(Height), float64(Height)))
+	imd.Rectangle(0.0)
+
+	size := (float64(Height) - (2*Border) + Gaps)/ 8.0
+	for y:=0;y<8;y++ {
+		for x:=0;x<8;x++ {
+			posX := float64(x)*size + Border
+			posY := float64(y)*size + Border
+			if (x+y)%2 == 0 {
+				imd.Color = TileA
+			} else {
+				imd.Color = TileB
+			}
+			imd.Push(pixel.V(posX, posY), pixel.V(posX+size-Gaps, posY+size-Gaps))
+			imd.Rectangle(0.0)
+		}
+	}
+}
+
+func drawPieces(b *board.BoardState, imd *imdraw.IMDraw){
+	size := (float64(Height) - (2*Border) + Gaps)/ 8.0
+	for y:=0;y<8;y++ {
+		for x:=0;x<8;x++ {
+			posX := float64(x)*size + Border
+			posY := float64(y)*size + Border
+
+			tile, _ := b.GetBoardTile(x,y)
+			if tile.Full == board.Empty { continue }
+			
+			imd.Color = color.RGBA{0x00, 0x00, 0x00, 0xFF}
+			imd.Push(pixel.V(posX+InternalGap, posY+InternalGap), pixel.V(posX+size-InternalGap, posY+size-InternalGap))
+			imd.Ellipse(pixel.V(0.0, 0.0), 0)
+			//imd.Push(pixel.V(200, 500), pixel.V(800, 500))
+			//imd.Ellipse(pixel.V(120, 80), 0)
+
+			if tile.Team == board.White {
+				imd.Color = color.RGBA{0xFF, 0xF9, 0xF4, 0xFF}
+			} else {
+				imd.Color = color.RGBA{0xC4, 0x00, 0x03, 0xFF}
+			}
+			imd.Push(pixel.V(200, 500), pixel.V(800, 500))
+			imd.Ellipse(pixel.V(120, 80), 0)
+		}
+	}
+}
+
+func drawControls(){
+
+}
--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"TurkishDraughts/Network"
-	"TurkishDraughts/Board"
+	"TurkishDraughts/UI"
+	"github.com/faiface/pixel/pixelgl"
 )
 
 
-
 func main() {
+	pixelgl.Run(ui.Init)
 	//network.Init()
-
 	
 	//Ai plays against itself for my amusement
-	b := board.CreateStartingBoard()
-	for true {
-		b = *(network.Analyze(b, 10))
-		b.Print()
-		winner, _ := b.PlayerHasWon()
-		if winner { break }
-	}
+	//b := board.CreateStartingBoard()
+	//for true {
+	//	b = *(network.Analyze(b, 10))
+	//	b.Print()
+	//	winner, _ := b.PlayerHasWon()
+	//	if winner { break }
+	//}
+
+	//b := network.ParseHistory("a3-a4 h6-h5 g3-g4 b6-b5 b3-a3 b5-a5")
+	//b.Print()
 }
+
+
+
