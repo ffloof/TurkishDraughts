0e98607943f32ea9b0b9f7a250de54fb184ef50f
Afel <prteza@gmail.com>
2021-11-20 17:25:54 -0800

    Commented moves.go and takes.go
    Wow that was painful

--- Board/moves.go
+++ Board/moves.go
@@ -1,20 +1,26 @@
 package board
 
+//Takes a king at a certain position and finds all the possible resulting boards
 func (bs *BoardState) AllMovesKing(x int, y int) []BoardState {
 	boards := []BoardState{}
 	checkingTile, _ := bs.GetBoardTile(x,y)
 
+	//Loops through every direction it could move
 	for _, direction := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} { //Right
 		for i:=1;i<8;i++ {
+			//Since its a king it can move i in any direction
 			move := [2]int{direction[0]*i, direction[1]*i}
 			moveTile, onBoard := bs.GetBoardTile(x + move[0],y + move[1])
+
+			//If the tile its trying to move to is empty add the valid move 
+			//otherwise we hit a wall, stop and try the next direction
 			if moveTile.Full == Empty && onBoard {
 				newBS := *bs
 				newBS.SetBoardTile(x + move[0], y + move[1], checkingTile)
 				newBS.SetBoardTile(x, y, Tile{})
 				boards = append(boards, newBS)
 			} else {
-				break //Stops going in this direction after it hits wall or piece
+				break //Hit wall, next direction
 			}
 		}
 	}
@@ -22,14 +28,18 @@ func (bs *BoardState) AllMovesKing(x int, y int) []BoardState {
 	return boards
 }
 
+//Takes a pawn at a certain position and finds all the possible resulting boards
 func (bs *BoardState) AllMovesPawn(x int, y int) []BoardState {
 	boards := []BoardState{}
 	checkingTile, _ := bs.GetBoardTile(x,y)
 
+	//Look at each possible direction it could move
 	for _, move := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} {
-		if checkingTile.Team == White && move[0] == 0 && move[1] == 1 { continue }
-		if checkingTile.Team == Black && move[0] == 0 && move[1] == -1 { continue }
+		//Check that pawns aren't moving backwards
+		if checkingTile.Team == White && move[1] == 1 { continue }
+		if checkingTile.Team == Black && move[1] == -1 { continue }
 		
+		//Checks if the move is valid and appends the possible board
 		moveTile, onBoard := bs.GetBoardTile(x + move[0],y + move[1])
 		if moveTile.Full == Empty && onBoard {
 			newBS := *bs
@@ -42,12 +52,17 @@ func (bs *BoardState) AllMovesPawn(x int, y int) []BoardState {
 	return boards
 }
 
+
+//Gets all possible moves that could be made on a turn
 func (bs *BoardState) AllMoveBoards() []BoardState {
 	possibleMoveBoards := []BoardState {}
+	//Loop through all pieces
 	for i:=0;i<64;i++ {
 		piece, _ := bs.GetBoardTile(i%8,i/8)
-		if piece.Full == Empty { continue }
-		if piece.Team != bs.Turn { continue }
+		//Skips pieces that are empty or are from the team color that isn't moving
+		if piece.Full == Empty || piece.Team != bs.Turn { continue } 
+		
+		//Adds possible moves each piece could make
 		if piece.King == King {
 			possibleMoveBoards = append(possibleMoveBoards, bs.AllMovesKing(i%8,i/8)...)
 		} else {
--- Board/takes.go
+++ Board/takes.go
@@ -1,19 +1,25 @@
 package board
 
+//Find all possible takes that result in the maximum amount
 func (bs *BoardState) MaxTakeBoards() []BoardState {
 	possibleMaxTakeBoards := []BoardState{}
 	bestTake := 1 //Filters boards with no jumps
 
+	//Loop through all pieces
 	for i:=0;i<64;i++ {
 		piece, _ := bs.GetBoardTile(i%8,i/8)
+		//Skip pieces that are empty or from the team not moving
 		if piece.Full == Empty || piece.Team != bs.Turn { continue }
 		var takes int
 		var possibleTakeBoards []BoardState
+		//Recursively find final outcomes of every take combination
 		if piece.King == King {
 			takes, possibleTakeBoards, _ = bs.FindKingTakes(i%8,i/8,0,[2]int{0,0})
 		} else {
 			takes, possibleTakeBoards, _ = bs.FindPawnTakes(i%8,i/8,0)
 		}
+
+		//Only combinations with the most takes are kept
 		if takes > bestTake {
 				bestTake = takes
 				possibleMaxTakeBoards = possibleTakeBoards
@@ -24,21 +30,25 @@ func (bs *BoardState) MaxTakeBoards() []BoardState {
 	return possibleMaxTakeBoards
 }
 
+//Finds all possible takes from a king at a given position
+//Returns # of takes, final boards, and if at starting depth what moves lead to max take outcomes (used in ui code)
 func (bs *BoardState) FindKingTakes(x int, y int, currentTakes int, lastDir [2]int) (int, []BoardState, []int) {
-	boards := []BoardState{}
+	newTakeBoards := []BoardState{}
 	bestTake := currentTakes
 	attackingTile, _ := bs.GetBoardTile(x,y)
 	validTakePos := []int{}
 
+	//Loop through each possible direction king can take in
 	for _, direction := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} {
+		//King can't take in a 180 from previous take (so it cant take, north then take south)
 		if -lastDir[0] == direction[0] && -lastDir[1] == direction[1] { continue } //Check to not go backwards in a direction
 
 		var jumpPos [2]int
 		var jumpOverTile Tile
 
+		//Searches in the direction until it finds a piece to jump over
 		i:=1
-		exit := false
-
+		exit := false //Exit indicates its either hit a friendly piece or a wall and that it should search the next direction
 		for i<8{
 			var onBoard bool
 			jumpPos = [2]int{x+(direction[0]*i),y+(direction[1]*i)}
@@ -47,96 +57,108 @@ func (bs *BoardState) FindKingTakes(x int, y int, currentTakes int, lastDir [2]i
 			i++
 
 			if jumpOverTile.Full == Empty { continue }
-			if !onBoard || attackingTile.Team == jumpOverTile.Team { 
-				exit = true
-				break
-			}
+			//Hit friendly piece or is searching off the board
+			if !onBoard || attackingTile.Team == jumpOverTile.Team { exit = true }
 			break
 		}
 		
-		if exit { continue }
+		if exit { continue } //Search next direction
 		
-		//We have a jump
+		//We have a possible piece to take
+		//Search each tile behind it to see if theres valid tile to land on, without jumping over multiple pieces
 		for i<8 {
 			landingPos := [2]int{x+(direction[0]*i), y+(direction[1]*i)} 
 			landingTile, onBoard := bs.GetBoardTile(landingPos[0], landingPos[1])
 
 			i++
 
-			if !onBoard || landingTile.Full == Filled { 
-				exit = true
-				break 
-			}
+			//Cant jump over multiple pieces or off the board so stops
+			if !onBoard || landingTile.Full == Filled { break }
 			
+			//Otherwise it has a valid take position and stores the board
 			newBS := *bs
 			newBS.SetBoardTile(landingPos[0], landingPos[1], attackingTile)
 			newBS.SetBoardTile(jumpPos[0], jumpPos[1], Tile{})
 			newBS.SetBoardTile(x,y, Tile{})
 
+			//Check if from that position there are any new takes recusrively
 			takes, possibleBoards, _ := newBS.FindKingTakes(landingPos[0], landingPos[1],currentTakes+1, direction)
 			
+			//Store only the boards that had the most amount of takes
 			if takes > bestTake {
-				if currentTakes == 0 {
+				if currentTakes == 0 { //If this is the starting node store the move made (for ui)
 					validTakePos = []int{(landingPos[1]*8)+landingPos[0]}
 				}
 				bestTake = takes
-				boards = possibleBoards
+				newTakeBoards = possibleBoards
 			} else if takes == bestTake {
-				if currentTakes == 0 {
+				if currentTakes == 0 { //If this is the starting node store the move made (for ui)
 					validTakePos = append(validTakePos, (landingPos[1]*8)+landingPos[0])
 				}
-				boards = append(boards, possibleBoards...)
+				newTakeBoards = append(newTakeBoards, possibleBoards...)
 			}
 		}		
 	}
 
-	if len(boards) == 0 {
+	if len(newTakeBoards) == 0 { //If there weren't any new takes found return end node
 		return bestTake, []BoardState{ *bs }, validTakePos
 	}
-	return bestTake, boards, validTakePos
+	//Otherwise return all the takes that resulted in the max amount of pieces taken searched and the amount
+	return bestTake, newTakeBoards, validTakePos 
 }
 
+//Finds recursively all possible takes from a pawn at a given position
+//Returns # of takes, final boards, and if at starting depth what moves lead to max take outcomes (used in ui code) 
 func (bs *BoardState) FindPawnTakes(x int, y int, currentTakes int) (int, []BoardState, []int) {
-	boards := []BoardState{}
+	newTakeBoards := []BoardState{}
 	bestTake := currentTakes
 	attackingTile, _ := bs.GetBoardTile(x,y)
 	validTakePos := []int{}
 	
+	//Loops through each direction a pawn could take in
 	for _, move := range [4][2]int {{0,1},{0,-1},{-1,0},{1,0},} {
+		//Pawns can't take backwards
 		if attackingTile.Team == White && (move[0] == 0 && move[1] == 1) { continue } //Down (black only)
 		if attackingTile.Team == Black && (move[0] == 0 && move[1] == -1) { continue } //Up (white only)
 
+		//Gets positions and tiles that are being jumped over and to
 		jumpPos := [2]int{ x+move[0],y+move[1] }
 		landingPos := [2]int { x+(2*move[0]) , y+(2*move[1]) }
 		jumpOverTile, onBoard1 := bs.GetBoardTile(jumpPos[0], jumpPos[1])
 		landingTile, onBoard2 := bs.GetBoardTile(landingPos[0], landingPos[1])
+
+		//Check if the take is valid
 		if onBoard1 && onBoard2 {
 			if landingTile.Full == Empty && jumpOverTile.Full == Filled && attackingTile.Team != jumpOverTile.Team {
+				//If it is create a new board that stores the outcome of that take
 				newBS := *bs
 				newBS.SetBoardTile(landingPos[0], landingPos[1], attackingTile)
 				newBS.SetBoardTile(jumpPos[0], jumpPos[1], Tile{})
 				newBS.SetBoardTile(x,y, Tile{})
 				
+				//Then check again if theres any possible takes from that board at the pawns new position
 				takes, possibleBoards, _ := newBS.FindPawnTakes(landingPos[0], landingPos[1],currentTakes+1)
+
+				//Only store the max take combinations
 				if takes > bestTake {
-					if currentTakes == 0 {
+					if currentTakes == 0 { //If this is the starting node store the move made (for ui)
 						validTakePos = []int{(landingPos[1]*8)+landingPos[0]}
 					}
 					bestTake = takes
-					boards = possibleBoards
+					newTakeBoards = possibleBoards
 				} else if takes == bestTake {
-					if currentTakes == 0 {
+					if currentTakes == 0 { //If this is the starting node store the move made (for ui)
 						validTakePos = append(validTakePos, (landingPos[1]*8)+landingPos[0])
 					}
-					boards = append(boards, possibleBoards...)
+					newTakeBoards = append(newTakeBoards, possibleBoards...)
 				}
 			}
 		}
 	}
 
-	if len(boards) == 0 {
+	if len(newTakeBoards) == 0 { //If there weren't any new takes found return end node
 		return bestTake, []BoardState{ *bs }, validTakePos
 	}
-	return bestTake, boards, validTakePos
-	
+	//Otherwise return all the takes that resulted in the max amount of pieces taken searched and the amount
+	return bestTake, newTakeBoards, validTakePos
 }
