bce0a040fabd45715f3c4e7a7318291060d5710f
Afel <prteza@gmail.com>
2021-11-21 16:57:10 -0800

    Cleaned up draw

--- UI/draw.go
+++ UI/draw.go
@@ -16,47 +16,6 @@ import (
 
 var currentTheme DrawTheme =  theme.LichessTheme{}
 
-var (
-	Border = 20.0
-	Gaps = 4.0
-	
-	BoardBg = color.RGBA{0xED, 0xEB, 0xE9, 0xFF}
-	TileDark = color.RGBA{0xB5, 0x88, 0x63, 0xFF}
-	TileLight = color.RGBA{0xF0, 0xD9, 0xB5, 0xFF}
-	
-	TeamWhite = color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}
-	TeamBlack = color.RGBA{0x00, 0x00, 0x00, 0xFF}
-
-	TakeColor = color.RGBA{0xFF, 0x00, 0x00, 0x7F}
-	MoveColor = color.RGBA{0x00, 0x3F, 0x00, 0x7F}
-
-	TextColor = color.RGBA{0x00, 0x00, 0x00, 0xFF}
-
-	CornerSize = 20.0
-
-	HoverSize = 2.0
-
-	InternalGap = 4.0
-)
-
-func getTileSpace() float64 {
-	return (float64(Height) - (2*Border) + Gaps)/ 8.0
-}
-
-func getTileSize() float64{
-	return (getTileSpace() - Gaps)
-}
-
-func getTilePosBL(x int, y int) (float64, float64) {
-	return float64(x)*getTileSpace() + Border, float64(7-y)*getTileSpace() + Border
-	
-}
-
-func getTilePosCenter(x int, y int) (float64, float64) {
-	return (float64(x)+0.5)*getTileSpace() + Border, (float64(7-y)+0.5)*getTileSpace() + Border
-}
-
-
 func drawBoard(imd *imdraw.IMDraw){
 	currentTheme.DrawBoard(imd)
 }
@@ -68,7 +27,7 @@ func drawPieces(imd *imdraw.IMDraw, b *board.BoardState){
 func drawControls(imd *imdraw.IMDraw, win *pixelgl.Window, black bool, white bool){
 	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
 	basicTxt := text.New(pixel.V(Height+20, Height-30), basicAtlas)
-	basicTxt.Color = TextColor
+	basicTxt.Color = color.RGBA{0x00, 0x00, 0x00, 0xFF}
 	fmt.Fprintln(basicTxt, "[+,-] AI Depth:", board.MaxDepth)
 	fmt.Fprintln(basicTxt, "[1] Black AI Moves:", black)
 	fmt.Fprintln(basicTxt, "[2] White AI Moves:", white)
@@ -91,26 +50,4 @@ func drawMoves(imd *imdraw.IMDraw, index int, moveMap map[int][]int){
 
 func drawChecks(imd *imdraw.IMDraw, moveMap map[int][]int) {
 	currentTheme.DrawChecks(imd, moveMap)
-}
-
-func corners(imd *imdraw.IMDraw, x1, y1 float64){
-	size := getTileSize()
-	x2 := x1 + size
-	y2 := y1 + size
-
-	//Bottom left corner
-	imd.Push(pixel.V(x1+CornerSize,y1), pixel.V(x1,y1), pixel.V(x1,y1+CornerSize))
-	imd.Polygon(0)
-
-	//Bottom right corner
-	imd.Push(pixel.V(x2-CornerSize,y1), pixel.V(x2,y1), pixel.V(x2,y1+CornerSize))
-	imd.Polygon(0)
-
-	//Top left corner
-	imd.Push(pixel.V(x1+CornerSize,y2), pixel.V(x1,y2), pixel.V(x1,y2-CornerSize))
-	imd.Polygon(0)
-
-	//Top right corner
-	imd.Push(pixel.V(x2-CornerSize,y2), pixel.V(x2,y2), pixel.V(x2,y2-CornerSize))
-	imd.Polygon(0)
 }
