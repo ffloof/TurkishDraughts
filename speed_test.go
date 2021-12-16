36da1f199e44ce0c017dae1857c0ec721053db38
Afel <prteza@gmail.com>
2021-12-16 05:09:41 -0800

    Added the improved test from the monte carlo branch
    That experiment was a disaster, onto reversi/othello for mcts. Ironically the mcts worked fine, it was the minmax that was bugging me the most.

--- speed_test.go
+++ speed_test.go
@@ -3,49 +3,116 @@ package main
 import (
 	"testing"
 	"time"
+	"runtime/debug"
 	"TurkishDraughts/Board"
 )
 
-func benchmarkDepths(t *testing.T, b *board.BoardState, testDepth int32) {
+func benchmarkDepthsMM(t *testing.T, b board.BoardState, testDepth int32) float64 {
 	startTime := time.Now()
-	board.Hits = 0
 	board.Searches = 0
-	
-	board.MaxDepth = testDepth
-	board.MaximumHashDepth = testDepth - 2
 
-	value := b.MinMax(0, -board.AlphaBetaMax, board.AlphaBetaMax, board.NewTable())
+	value := b.MinMax(0, -999.0, 999.0, board.NewTable())
 
 	duration := time.Since(startTime).Seconds()
 	t.Log("---", testDepth)
 	t.Log("Time:", float32(duration), "s")
-	t.Log("Hits:", board.Hits/1000, "k  ", int(float64(board.Hits)/duration/1000.0),"k/s")
+	t.Log("Value:", value)
 	t.Log("Searches:", board.Searches/1000, "k  ",int(float64(board.Searches)/duration/1000.0),"k/s")
-	t.Log("Standing:", value)
+
+	//Forces memory to get cleared immediately so it will be ready for next test
+	debug.FreeOSMemory()
+	return duration
 }
 
-func TestBenchStartBoard(t *testing.T){
-	//Config
-	board.AdvanceWeight = 0.1
-	board.TableDepthAllowedInaccuracy = 0
 
-	var i int32 = 5
-	for i<=12 {
-		b := board.CreateStartingBoard()
-		benchmarkDepths(t, &b, i)
-		i++
-	}	
-}
 
-func TestBenchNoAdvanced(t *testing.T){
-	//Config
-	board.AdvanceWeight = 0.0
-	board.TableDepthAllowedInaccuracy = 0
+//Vanilla, default configuration
+func TestBenchMMVanilla(t *testing.T){
+	var i int32 = 7
+	var lasttime float64
+	for {
+		//Config
+		board.AdvanceWeight = 0.0
+		board.TableDepthAllowedInaccuracy = 0
+		board.MaxDepth = i
+		board.MaximumHashDepth = i
 
-	var i int32 = 5
-	for i<=12 {
-		b := board.CreateStartingBoard()
-		benchmarkDepths(t, &b, i)
+		lasttime = benchmarkDepthsMM(t, board.CreateStartingBoard(), i)
+		if lasttime > 5.0 { break }
 		i++
 	}
+	t.Log("===", i)
+}
+
+//NoTable, doesn't use transposition table
+func TestBenchMMNoTable(t *testing.T){
+	var i int32 = 7
+	var lasttime float64
+	for i<=12 {
+		//Config
+		board.AdvanceWeight = 0.0
+		board.TableDepthAllowedInaccuracy = 0
+		board.MaxDepth = i
+		board.MaximumHashDepth = 0
+
+		lasttime = benchmarkDepthsMM(t, board.CreateStartingBoard(), i)
+		if lasttime > 5.0 { break }
+		i++
+	}
+	t.Log("===", i)
+}
+
+
+//FastTable, uses a table with settings slightly optimized
+func TestBenchMMFastTable(t *testing.T){
+	var i int32 = 7
+	var lasttime float64
+	for {
+		//Config
+		board.AdvanceWeight = 0.0
+		board.TableDepthAllowedInaccuracy = 0
+		board.MaxDepth = i
+		board.MaximumHashDepth = i-2
+
+		lasttime = benchmarkDepthsMM(t, board.CreateStartingBoard(), i)
+		if lasttime > 5.0 { break }
+		i++
+	}
+	t.Log("===", i)
+}
+
+//Advanced, default configuration + advance heuristic for evaluation
+func TestBenchMMAdvanced(t *testing.T){
+	var i int32 = 7
+	var lasttime float64
+	for {
+		//Config
+		board.AdvanceWeight = 0.1
+		board.TableDepthAllowedInaccuracy = 0
+		board.MaxDepth = i
+		board.MaximumHashDepth = i
+
+		lasttime = benchmarkDepthsMM(t, board.CreateStartingBoard(), i)
+		if lasttime > 5.0 { break }
+		i++
+	}
+	t.Log("===", i)
+}
+
+//CheatTable lets transposition table cheat slightly by looking at shallower depths
+func TestBenchMMCheatTable(t *testing.T){
+	var i int32 = 7
+	var lasttime float64
+	for {
+		//Config
+		board.AdvanceWeight = 0.0
+		board.TableDepthAllowedInaccuracy = 2
+		board.MaxDepth = i
+		board.MaximumHashDepth = i - 2
+
+		lasttime = benchmarkDepthsMM(t, board.CreateStartingBoard(), i)
+		if lasttime > 5.0 { break }
+		i++
+	}
+	t.Log("===", i)
 }
