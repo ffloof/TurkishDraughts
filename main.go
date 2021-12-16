b54b6a1326e1a2d8634f6c1facf5840c23177050
Afel <prteza@gmail.com>
2021-12-16 05:14:33 -0800

    The nightmares they're all over

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	//"TurkishDraughts/Tournament"
-	//"TurkishDraughts/UI"
-	//"github.com/faiface/pixel/pixelgl"
+	"TurkishDraughts/UI"
+	"github.com/faiface/pixel/pixelgl"
 
 	"TurkishDraughts/Board"
 	"fmt"
@@ -11,23 +11,8 @@ import (
 
 
 func main() {
-	//pixelgl.Run(ui.Init)
+	pixelgl.Run(ui.Init)
 	//tournament.Run()
-
-	b1 := board.BoardFromStr("-------- bbbbbbbb -bbbbbbb b------- -ww----- w--wwwww wwwwwwww --------")
-
-	b2 := board.BoardFromStr("-------- bbbbbbbb --bbbbbb bb------ -ww----- w--wwwww wwwwwwww --------")
-	b2.SwapTeam()
-	v1, _ := b1.MinMax(0,-999.0,999.0,board.NewTable())
-	v2, _ := b2.MinMax(1,-999.0,999.0,board.NewTable())
-
-	fmt.Println(v1)
-	//bs1[1].Print()
-	fmt.Println()
-
-	fmt.Println(v2)
-	//bs2[0].Print()
-	fmt.Println()
 }
 
 
